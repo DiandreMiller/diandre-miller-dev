0f3b76439d4492924296a318e4f37ca309615266
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2025-08-31 18:24:47 -0400

    inital about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <h1>About Me</h1>
+      <p>
+        My name is Diandre Miller, and I’m a software developer from Brooklyn. I began my career at a nonprofit called East Flatbush Village, Inc., where I served as both an Administrative Director and an Assistant Program Director. I truly loved the work I did there. Unfortunately, I was forced to step away due to a health issue.
+        During that time away, I taught myself video editing. That was a big deal for me because, before that, I struggled with technology—I couldn’t even figure out how to connect my phone to Bluetooth. But learning video editing on my own gave me the confidence to believe I could do something even more technical, like software development.
+        That belief led me to join a one-year bootcamp called Pursuit, where I developed the skills to become a software developer. I learned multiple programming languages, worked with various frameworks and libraries, and built several projects.
+        What I love most about being a software developer is the ability to create. If there's something I feel is missing in the world, I have the power to build it. That’s an incredibly empowering feeling—being able to solve real-world problems through code.
+      </p>
+    </div>
   )
 }
 
