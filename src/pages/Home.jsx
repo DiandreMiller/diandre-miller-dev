f8bfed9210408ca9ff22c4001f777a89c361371c
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2025-08-30 20:00:18 -0400

    tailwind successful installed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import DiandreLogo from '../assets/diandre-miller-dev.png'
 const Home = () => {
   return (
     <div>
-        <h1>Home Page</h1>
+        <h1 className='className="text-4xl font-bold text-blue-500'>Home Page</h1>
         <img src={DiandreLogo} />
         
     </div>
